51a2f0a894a44e95c7d279eea2a6e7386bd2b7c2
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-08-14 19:13:08 -0700

    🌻UPDATE: updated socketurl

--- client/src/config/prod.ts
+++ client/src/config/prod.ts
@@ -2,7 +2,7 @@ import { SingleConfig } from '@/config/index'
 
 const production: SingleConfig = {
 	serverUrl: 'https://emoji-io-api.herokuapp.com',
-    socketUrl: 'ws://emoji-io-api.herokuapp.com'
+    socketUrl: 'wss://emoji-io-api.herokuapp.com'
 }
 
 export default production
