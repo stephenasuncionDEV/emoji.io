1f7abb6bdd532f1fcd09cb2d698e26d8a70ccb7d
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-08-14 21:27:22 -0700

    🌻UPDATE: updated profile modal

--- client/src/components/ProfileModal.tsx
+++ client/src/components/ProfileModal.tsx
@@ -1,7 +1,7 @@
 import { Text, Flex, HStack, Button, Modal, ModalOverlay, 
     ModalContent, ModalHeader, ModalFooter, ModalBody, 
     ModalCloseButton, VStack, Box, Tag, TagLabel, 
-    TagRightIcon, Input, Wrap
+    TagRightIcon, Input, Wrap, Icon, Spinner
 } from '@chakra-ui/react'
 import { FC } from 'react'
 import { FaSave } from 'react-icons/fa'
@@ -34,21 +34,45 @@ const ProfileModal: FC<ProfileProps> = ({ isProfileOpen, onProfileClose }) => {
                 <ModalBody display='flex' gap='1.5em'>
                     <VStack>
                         <Box p='.5em' border='2px dashed rgb(120,120,120)' borderRadius='5px'>
-                            <Text fontSize='48pt'>
-                                {emoji}
-                            </Text>
+                            {emoji ? (
+                                <Text fontSize='48pt'>
+                                    {emoji}
+                                </Text>
+                            ) : (
+                                <Box p='1.25em'>
+                                    <Spinner
+                                        thickness='4px'
+                                        speed='0.65s'
+                                        emptyColor='gray.200'
+                                        color='orange.500'
+                                        size='xl'
+                                    />
+                                </Box>
+                            )}
                         </Box>
                     </VStack>
                     <Flex flexDir='column' flex='1'>
                         <Flex justifyContent='flex-end'>
-                            <Tag justifyContent='space-between' size='sm' maxW='140px'>
-                                <TagLabel>
-                                    {user?._id}
-                                </TagLabel>
-                                <TagRightIcon cursor='pointer' onClick={onCopy}>
-                                    <BiCopy fontSize='18pt' color='gray' />
-                                </TagRightIcon>
-                            </Tag>
+                            <HStack>
+                                <Text fontSize='9pt'>
+                                    ID
+                                </Text>
+                                <Tag justifyContent='space-between' size='sm' maxW='140px'>
+                                    <TagLabel>
+                                        {user?._id}
+                                    </TagLabel>
+                                    <TagRightIcon 
+                                        cursor='pointer' 
+                                        onClick={onCopy} 
+                                        color='gray.400' 
+                                        _hover={{
+                                            color: 'gray.500'
+                                        }}
+                                    >
+                                        <BiCopy fontSize='18pt' />
+                                    </TagRightIcon>
+                                </Tag>
+                            </HStack>
                         </Flex>
                         <VStack>
                             <Input type='text' value={name} mt='1em' size='sm' onChange={(e) => setName(e.target.value)} />
@@ -58,8 +82,24 @@ const ProfileModal: FC<ProfileProps> = ({ isProfileOpen, onProfileClose }) => {
                             Owned Emojis
                         </Text>
                         <Wrap spacing='.5em' mt='.5em'>
+                            <Button 
+                                size='sm' 
+                                fontSize='12pt' 
+                                variant='primary' 
+                                onClick={() => onSetEmoji('🐀')} 
+                                disabled={user?.player?.emoji === '🐀'}
+                            >
+                                🐀
+                            </Button>
                             {user?.player?.emojiOwned?.map((emoji, idx) => (
-                                <Button size='sm' fontSize='12pt' variant='primary' onClick={() => onSetEmoji(emoji)} key={idx}>
+                                <Button 
+                                    size='sm' 
+                                    fontSize='12pt' 
+                                    variant='primary' 
+                                    onClick={() => onSetEmoji(emoji)} 
+                                    key={idx}
+                                    disabled={user?.player?.emoji === emoji}
+                                >
                                     {emoji}
                                 </Button>
                             ))}
@@ -76,6 +116,7 @@ const ProfileModal: FC<ProfileProps> = ({ isProfileOpen, onProfileClose }) => {
                             size='sm' 
                             leftIcon={<FaSave />} 
                             variant='primary'
+                            disabled={name === user?.name}
                         >
                             Save
                         </Button>
